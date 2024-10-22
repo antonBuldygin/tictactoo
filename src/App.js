87e93b38858d7084cc2ca5baa04bcd54f34ace7d
antonBuldygin <anton.buldygin80@mail.ru>
2024-10-22 14:42:15 +0300

    Hi

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1"/>
+        <Square value="1"/>
+        <Square value="1"/>
       </div>
     </>
   );
 }
-function Square() {
-  return <button className="square">1</button>;
+function Square({ value }) {
+  function handleClick() {
+    console.log("URA");
+
+}
+
+  return <button className="square" onClick={handleClick}>{ value}</button>;
 }
