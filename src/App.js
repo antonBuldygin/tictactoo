1db8e3a7feaa625908ac6403b48b4868a6b8c4ee
antonBuldygin <anton.buldygin80@mail.ru>
2024-10-22 20:52:56 +0300

    qwqwqw

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+import { useState } from "react";
+
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="14"/>
-        <Square value="1"/>
-        <Square value="1"/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null);
   function handleClick() {
+    setValue('x');
     console.log("URA");
 
 }
